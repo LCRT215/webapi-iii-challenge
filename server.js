fd6bd3046e6d83e1560aaccc3710174f8e08bf97
Leslie Thompson <lcrthompson@gmail.com>
2019-05-16 07:47:42 -0700

    rewrite logger function

--- server.js
+++ server.js
@@ -10,13 +10,17 @@ server.use(helmet());
 server.use(morgan("dev"));
 
 server.get("/", (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2>hey, Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-  console.log(req.method, "...this logger");
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      "Origin"
+    )}`
+  );
   next();
 }
 
