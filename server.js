f2befba05c3417629a0a6b10280ca768f56ef8e9
Leslie Thompson <lcrthompson@gmail.com>
2019-05-15 21:19:43 -0700

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const server = express();
 
 server.use(logger);
+server.use(express.json());
+server.use(helmet());
+server.use(morgan("dev"));
 
 server.get("/", (req, res) => {
-  res.send(`<h2>Let's write some middleware! hey</h2>`);
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
